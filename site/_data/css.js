bca531f739c9093d78a35ce990906571c9bbf9a5
syoung6 <sam.young@mailbox.org>
2021-03-09 13:34:10 -0600

    Update: remove keystone code

--- site/_data/css.js
+++ site/_data/css.js
@@ -6,10 +6,10 @@ const { compileSassTargets } = require("../utils/compile-scss");
 // Add SCSS compilation targets here
 const targets = {
   main: "site/src/scss/main.scss",
-  "keystone-comments": "site/src/scss/keystone-comments.scss",
-  "keystone-claps": "site/src/scss/keystone-claps.scss",
-  "keystone-login": "site/src/scss/keystone-login.scss",
-  "keystone-bookmarks": "site/src/scss/keystone-bookmarks.scss"
+  // "keystone-comments": "site/src/scss/keystone-comments.scss",
+  // "keystone-claps": "site/src/scss/keystone-claps.scss",
+  // "keystone-login": "site/src/scss/keystone-login.scss",
+  // "keystone-bookmarks": "site/src/scss/keystone-bookmarks.scss"
 };
 
 // Create an object with the compiled CSS for each key in targets
--- site/_data/site.js
+++ site/_data/site.js
@@ -8,25 +8,25 @@ module.exports = {
   authorName: "Mike",
   postsPerPage: 4,
   socialImage: "/img/social.jpg",
-  theme: {
-    primary: {
-      background: "white",
-      text: "black",
-      highlight: "#666",
-    },
-    secondary: {
-      background: "black",
-      text: "white",
-      highlight: "#666",
-    },
-  },
+  // theme: {
+  //   primary: {
+  //     background: "white",
+  //     text: "black",
+  //     highlight: "#666",
+  //   },
+  //   secondary: {
+  //     background: "black",
+  //     text: "white",
+  //     highlight: "#666",
+  //   },
+  // },
 
-  keystone: {
-    comments: true,
-    bookmarks: true,
-    claps: true,
-    login: true,
-  },
+  // keystone: {
+  //   comments: true,
+  //   bookmarks: true,
+  //   claps: true,
+  //   login: true,
+  // },
   // Critical CSS results in much slower build times and uses a lot of system resources
   // turn on in production :)
   // See `site/transforms/critical-css-transform.js` for more details
